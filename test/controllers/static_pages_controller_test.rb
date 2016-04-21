d97cace08759ebfb0ad6f7e370856f54295b6b4b
rodrigopk <rodrigopk@gmail.com>
2016-04-21 14:26:30 +0000

    Adicionando fullTitle helper

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Início | BitInvest"
+    assert_select "title", "BitInvest"
   end
 
   test "should get help" do
