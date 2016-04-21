a091502e0e3c51bcfadfa0fad7291789105cdefe
rodrigopk <rodrigopk@gmail.com>
2016-04-21 21:30:01 +0000

    Implementando sessão de usuário e login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Bem vindo ao BitInvest!"
       redirect_to @user
     else
