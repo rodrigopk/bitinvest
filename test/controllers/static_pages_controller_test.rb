8556e4bf3851e8baf718e2174c5b5f0f65d5d8f8
rodrigopk <rodrigopk@gmail.com>
2016-04-21 13:51:11 +0000

    so deus sabe

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionController::TestCase
+  test "should get home" do
+    get :home
+    assert_response :success
+    assert_select "title", "Início | BitInvest"
+  end
+
+  test "should get help" do
+    get :help
+    assert_response :success
+    assert_select "title", "Ajuda | BitInvest"
+  end
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contato | BitInvest"
+  end
+
+end
