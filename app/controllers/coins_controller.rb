9851fb9e843997bc5f4cc97178d2cb0a944f636a
rodrigopk <rodrigopk@gmail.com>
2016-05-01 22:48:06 +0000

    Adicionando thumbnais as cryptocoins e novas moedas

--- app/controllers/coins_controller.rb
+++ app/controllers/coins_controller.rb
@@ -3,18 +3,17 @@ class CoinsController < ApplicationController
   require 'open-uri'
   
   def index
-#    @coins = []
-#    Coin.all.each do |coin|
-#    
-#      json_string = JSON.load(open("https://www.cryptonator.com/api/ticker/"+coin.symbol.downcase+"-usd"))
-#      ticker = json_string["ticker"]
-#      coin.value = ticker["price"].to_f
-#      coin.volume = ticker["volume"].to_f
-#      coin.save
-#      @coins.push(coin) 
-#      
-#    end
-    @coins = Coin.all 
+    @coins = []
+    Coin.all.each do |coin|
+    
+      json_string = JSON.load(open("https://www.cryptonator.com/api/ticker/"+coin.symbol.downcase+"-usd"))
+      ticker = json_string["ticker"]
+      coin.value = ticker["price"].to_f
+      coin.volume = ticker["volume"].to_f
+      coin.save
+      @coins.push(coin) 
+      
+    end
     #@coins = Coin.paginate(page: params[:page])
   end
   
--- db/seeds.rb
+++ db/seeds.rb
@@ -17,6 +17,11 @@ Coin.create(name: "Bitcoin",
             symbol: "BTC", 
             value: 448.60346080, 
             volume: 50200.90849329)
+
+Coin.create(name: "Ethereum", 
+            symbol: "ETH", 
+            value: 8.56808576, 
+            volume: 45086.97294231) 
             
 Coin.create(name: "Litecoin", 
             symbol: "LTC", 
@@ -26,4 +31,34 @@ Coin.create(name: "Litecoin",
 Coin.create(name: "Dogecoin", 
             symbol: "DOGE", 
             value: 0.00021856, 
-            volume: 13367483.16176800)            
+            volume: 13367483.16176800)
+            
+Coin.create(name: "Ripple", 
+            symbol: "XRP", 
+            value: 0.00688204, 
+            volume: 0)
+            
+Coin.create(name: "Dash", 
+            symbol: "DASH", 
+            value: 6.71728107, 
+            volume: 1716.08122255)
+            
+Coin.create(name: "Monero", 
+            symbol: "XMR", 
+            value: 0.90155156, 
+            volume: 0)
+            
+Coin.create(name: "MaidSafeCoin", 
+            symbol: "MAID", 
+            value: 0.05869990, 
+            volume: 0)
+
+Coin.create(name: "Namecoin", 
+            symbol: "NMC", 
+            value: 0.42900000, 
+            volume: 61607.31921000)
+
+Coin.create(name: "Feathercoin", 
+            symbol: "FTC", 
+            value: 0.03375246, 
+            volume: 0)            
