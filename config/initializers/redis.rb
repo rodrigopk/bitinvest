d76cb049ca8af0ed8ad046e6b214a304209d4a46
rodrigopk <rodrigopk@gmail.com>
2016-05-18 18:12:33 +0000

    redis.rb

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,9 +1,1 @@
-#config/initializers/redis.rb
-require 'redis'
-
-REDIS_CONFIG = YAML.load( File.open( Rails.root.join("config/redis.yml") ) ).symbolize_keys
-dflt = REDIS_CONFIG[:default].symbolize_keys
-cnfg = dflt.merge(REDIS_CONFIG[Rails.env.to_sym].symbolize_keys) if REDIS_CONFIG[Rails.env.to_sym]
-
-$redis = Redis.new(cnfg)
-Redis::Objects.redis = $redis
+$redis = Redis.new(:host => 'localhost', :port => 6379)
