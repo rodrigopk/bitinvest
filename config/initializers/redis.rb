4c2b7cb5f2816994674d5ac066c2a6ced0424511
rodrigopk <rodrigopk@gmail.com>
2016-05-18 18:09:09 +0000

    Testando config Redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,6 +1,5 @@
 #config/initializers/redis.rb
 require 'redis'
-require 'redis/objects'
 
 REDIS_CONFIG = YAML.load( File.open( Rails.root.join("config/redis.yml") ) ).symbolize_keys
 dflt = REDIS_CONFIG[:default].symbolize_keys
