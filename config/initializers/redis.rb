b7ff8eddff90c70ae25f275abbf2b8adf7c7bebc
rodrigopk <rodrigopk@gmail.com>
2016-05-18 18:07:25 +0000

    Testando config Redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,10 @@
-$redis = Redis::Namespace.new("ihms_env_app", :redis => Redis.new)
+#config/initializers/redis.rb
+require 'redis'
+require 'redis/objects'
 
-uri = URI.parse(ENV["REDISTOGO_URL"])
-REDIS = Redis.new(:url => ENV['REDISTOGO_URL'])
+REDIS_CONFIG = YAML.load( File.open( Rails.root.join("config/redis.yml") ) ).symbolize_keys
+dflt = REDIS_CONFIG[:default].symbolize_keys
+cnfg = dflt.merge(REDIS_CONFIG[Rails.env.to_sym].symbolize_keys) if REDIS_CONFIG[Rails.env.to_sym]
+
+$redis = Redis.new(cnfg)
+Redis::Objects.redis = $redis
