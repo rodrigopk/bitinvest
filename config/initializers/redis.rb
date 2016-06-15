5afe6b07e11f65c7c4dfc133c42d78baa3e216b7
rodrigopk <rodrigopk@gmail.com>
2016-06-15 03:10:38 +0000

    corrigindo redis.rb

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -2,7 +2,7 @@ conf_file = File.join('config','redis.yml')
 
 $redis = if File.exists?(conf_file)
   conf = YAML.load(File.read(conf_file))
-  conf[Rails.env.to_s].blank? ? Redis.new : Redis.new(conf[Rails.env.to_s])
+  conf[Rails.env.to_s].blank? ? Redis.new : Redis.new(conf[Rails.env])
 else
   Redis.new
 end
