b73f7c1c821eb2c737c13d1088f6edd01039f3cc
rodrigopk <rodrigopk@gmail.com>
2016-07-27 08:24:11 -0300

    Bug fix: reversed order at ranking

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,7 @@ class StaticPagesController < ApplicationController
   def home
   	if logged_in?
   		#Coin.order(:rank)[0..9]
-  	  @users = User.all.sort_by{|user| user.total_value_fiat}[0..9]
+  	  @users = User.all.sort_by{|user| user.total_value_fiat}.reverse![0..9]
   	end
   end
 
