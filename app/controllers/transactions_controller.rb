6dd81dd04b44842f5bfce500d44e50d64a6d5d84
rodrigopk <rodrigopk@gmail.com>
2016-08-25 14:38:15 -0300

    refatorando transaction_controller && adicionando verificacao de oferta disponivel

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,5 +1,9 @@
 class TransactionsController < ApplicationController
-  
+  before_action :get_source_wallet, only: [:create]
+  before_action :get_units, only: [:create]
+  before_action :coin_available_supply, only: [:create]
+
+
   def new
     @wallet = nil
     unless current_user.wallets.find_by(coin_id: params[:coin_id]).nil?
@@ -20,50 +24,23 @@ class TransactionsController < ApplicationController
   end
   
   def create
-    index = 0
-    current_user.wallets.each  { |wallet|
-      if wallet.id == params[:wallet_id].to_i
-        break
-      else
-        index = index+1 
-      end
-    }
-
+    
     fiat_wallet = current_user.get_fiat_wallet
-    units = 0
-    if params[:type] == 'sell'
-      units = -(params[:units].to_f)
-    else
-      units = params[:units].to_f
-    end
-    logger.debug "\n=== type: #{params[:type]} ===\n"
-    logger.debug "\n=== wallet: #{current_user.wallets[index].coin.name} ===\n"
-
-    fiat_units = fiat_wallet.units+(-1*units*current_user.wallets[index].coin.value)
-    source_units = current_user.wallets[index].units+units
+    transaction_value = @units*@user_wallet.coin.value
+    fiat_units = fiat_wallet.units+(-1*transaction_value)
+    source_units = @user_wallet.units+@units
 
     if ( params[:type] == 'sell' && source_units < 0 ) ||
         ( params[:type] == 'buy' && fiat_units < 0 )
       flash[:danger] = t(:insufficient)
-      redirect_to new_transaction_path(:type => params[:type],:coin_id => current_user.wallets[index].coin_id)
+      redirect_to new_transaction_path(:type => params[:type],:coin_id => @user_wallet.coin_id)
     else
       fiat_wallet.update_attribute(:units,fiat_units)
-      current_user.wallets[index].update_attribute(:units,source_units)
+      @user_wallet.update_attribute(:units,source_units)
 
-      #reward user
-      level = current_user.level
-      current_user.reward_xp(:small)
-      flash[:success] = t(:level_up) if current_user.level > level
-
-      #collect coin statistics
-      current_user.wallets[index].coin.coin_average_statistic.update_attribute(:total_volume,
-                        current_user.wallets[index].coin.coin_average_statistic.total_volume+(units*current_user.wallets[index].coin.value).abs)
-      current_user.wallets[index].coin.coin_average_statistic.increment!(:total_operations)
-      
-      #collect user statistics
-      current_user.update_attribute(:daily_volume,
-                        current_user.daily_volume+(units*current_user.wallets[index].coin.value).abs)
-      current_user.increment!(:daily_transactions)
+      reward_user()
+      collect_coin_statistics(@user_wallet.coin,transaction_value)
+      collect_user_statistics(transaction_value)
       
       redirect_to current_user  
     end
@@ -75,5 +52,43 @@ class TransactionsController < ApplicationController
     def get_fiat_value
       Coin.last.value
     end
-  
+
+    def reward_user
+      level = current_user.level
+      current_user.reward_xp(:small)
+      flash[:success] = t(:level_up) if current_user.level > level
+    end
+
+    def collect_coin_statistics(coin, transaction_value)
+      coin.coin_average_statistic.update_attribute(:total_volume,
+                                        coin.coin_average_statistic.total_volume+transaction_value.abs)
+      coin.coin_average_statistic.increment!(:total_operations)
+    end
+
+    def collect_user_statistics(transaction_value)
+      current_user.update_attribute(:daily_volume,
+                        current_user.daily_volume+transaction_value.abs)
+      current_user.increment!(:daily_transactions)
+    end
+
+    # actions/filters
+    def coin_available_supply 
+      if (params[:units].to_f  > @user_wallet.coin.available_supply) && params[:type] == 'buy'
+        flash[:danger] = t(:exceeds_supply)
+        redirect_to new_transaction_path(:type => params[:type],:coin_id => @user_wallet.coin_id)
+      end
+    end
+
+    def get_source_wallet
+      @user_wallet = current_user.wallets.where(id: params[:wallet_id].to_i).first
+    end
+
+    def get_units 
+      if params[:type] == 'sell'
+        @units = -(params[:units].to_f)
+      else
+        @units = params[:units].to_f
+      end
+    end
+
 end
