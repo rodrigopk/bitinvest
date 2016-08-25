cf44e7c159fcb0b1cefe4c11564cb2757dac7e74
rodrigopk <rodrigopk@gmail.com>
2016-08-25 13:00:55 -0300

    Fix: imagem nas carteiras

--- app/helpers/coins_helper.rb
+++ app/helpers/coins_helper.rb
@@ -1,4 +1,18 @@
 module CoinsHelper
+  require "net/http"
+
+  # def icon_url(tag)
+  #   url = URI.parse("https://coinmarketcap.com/static/img/coins/16x16/"+tag+".png")
+  #   req = Net::HTTP.new(url.host, url.port)
+  #   req.use_ssl = true
+  #   res = req.request_head(url.path)
+  #   if res.code == "200"
+  #     logger.debug "\n=== return : #{url.host+url.path} ====\n"
+  #     return url.to_s
+  #   else
+  #     return "coin_404_16.png"
+  #   end
+  # end
 
   def icon_url(tag)
     "https://coinmarketcap.com/static/img/coins/16x16/"+tag+".png"
